a0258c20020a17f12e125bc83493561355303607
limingyang <limingyang.666@bytedance.com>
2023-03-25 19:34:06 +0800

    fix: 空行

--- src/system.ts
+++ src/system.ts
@@ -9,7 +9,6 @@ export const setting = {
 
 export const message = `Powered by OpenAI 树莓小雪糕
 - 该网站仅作为演示，不提供长期服务，请勿滥用。
-- 
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
 export type Setting = typeof setting
