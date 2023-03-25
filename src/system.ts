73a18269767a8c45d6d79e0dfeb99dd93554e919
limingyang <limingyang.666@bytedance.com>
2023-03-25 19:28:17 +0800

    fix: 去掉starlogo

--- src/system.ts
+++ src/system.ts
@@ -9,7 +9,6 @@ export const setting = {
 
 export const message = `Powered by OpenAI Vercel
 - 该网站仅作为演示，不提供长期服务，，请勿滥用。
-- 该预览页面域名由 [@AUDI_GUZZ](https://m.okjike.com/users/4af3cfb4-1291-4a8b-b210-f515c86934a9) 和 [@Airyland](https://m.okjike.com/users/C6C8DE3A-E89D-4978-9E7D-B2E167D835A9) 免费提供，感谢。API Key 由我自己免费提供，注意不要滥用。
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
 export type Setting = typeof setting
